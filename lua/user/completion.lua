25855d306005c115ce8d5fc4c2eb0690078ecfa4
Vitalii <grust.87@gmail.com>
2022-06-04 08:54:14 -0400

    fixes after patch updates

--- lua/user/completion.lua
+++ lua/user/completion.lua
@@ -121,9 +121,10 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  },
+  window = {
+      -- completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
   experimental = {
     ghost_text = false,
     native_menu = false,
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,35 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {
+    "bash",
+    "cmake",
+    "css",
+    "comment",
+    "fish",
+    "go",
+    "graphql",
+    "html",
+    "http",
+    "javascript",
+    "jsdoc",
+    "json",
+    "json5",
+    "latex",
+    "lua",
+    "make",
+    "php",
+    "python",
+    "regex",
+    "scheme",
+    "scss",
+    "slint",
+    "toml",
+    "tsx",
+    "typescript",
+    "vim",
+    "yaml"
+  }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
