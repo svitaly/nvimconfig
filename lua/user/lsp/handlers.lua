064c047e706d2e51d1ef25a2f9a1a8899145e073
Vitalii <grust.87@gmail.com>
2022-07-16 17:13:22 -0400

    resolve mapping conflicts

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -79,7 +79,8 @@ local function lsp_keymaps(bufnr)
     opts
   )
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+  -- next line conflicts with 'smart quit' function
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
